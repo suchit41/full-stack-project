ef986e4d10bffc809e29412e9a5019bb0f3e30d0
suchit41 <suchitpravinjain@gmail.com>
2023-12-04 18:20:12 +0530

    user control is updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import {asyncHandler} from '../utils/asyncHandler.js'
 
 const registeruser = asyncHandler(async(req,res)=>{
     res.status(200).json({
-        message:"ok"
+        message:"okay"
     })
 })
 
